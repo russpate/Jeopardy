29d2266fc453ae9055a3167145943b58eb006c01
Russell Pate <russ@russpate.com>
2016-03-29 00:13:53 -0400

    polishing responsive, refining style, attempting to disable after submission, stripping down data to make it easier for users to answer, and updated score to be fixed position

--- jeopardyGameBoard/directives/tempDirective.js
+++ jeopardyGameBoard/directives/tempDirective.js
@@ -8,15 +8,25 @@ angular
         cats:'='
       },
       controller: function($rootScope, $scope){
+        //Andrew helped me with these two functions.
+
+        // Addscore is for tallying up a users total score
+        // while in game play
         $scope.addScore = function(input, answer, value){
-          if(input === answer){
+          //strip out html from answer https://css-tricks.com/snippets/javascript/strip-html-tags-in-javascript/
+          if(input === answer.toLowerCase().replace(/(<([^>]+)>)|'\'/ig,"")){
             $rootScope.score += value
           }else{
             $rootScope.score -= value
           };
         }
+        //hideModal closes the modal after the user has submitted their answer
         $scope.hideModal = function (id) {
           $('#' + id).modal('hide')
+        }
+        // got example from frank / hanks demo in class
+        $scope.disableButton = function(id){
+          $("div." + id).toggle();
         }
       }
     }
